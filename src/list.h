dad5a861aca07f6e11b392f8c22b55fbf5c321e0
Marek Majkowski <marek@popcount.org>
2013-09-08 14:45:45 +0100

    Remove warning in clang (offsetof redefined)

--- src/list.h
+++ src/list.h
@@ -9,11 +9,16 @@
 #define prefetch(a) __builtin_prefetch(a)
 
 #ifndef container_of
-#define container_of(ptr, type, member) ({      \
-	const typeof( ((type *)0)->member ) *__mptr = (ptr);  \
-	(type *)( (char *)__mptr - offsetof(type,member) );})
-#define offsetof(TYPE, MEMBER) ((size_t) &((TYPE *)0)->MEMBER)
+# define container_of(ptr, type, member)				\
+	({								\
+		const typeof( ((type *)0)->member ) *__mptr = (ptr);	\
+		(type *)( (char *)__mptr - offsetof(type,member) );	\
+	})
 #endif //container_of
+
+#ifndef offsetof
+# define offsetof(TYPE, MEMBER) ((size_t) &((TYPE *)0)->MEMBER)
+#endif
 
 //#include <linux/prefetch.h>
 //#include <asm/system.h>
